42044af04cd8e3320712a5649d4957bf33f941a1
Santiago Aranguri <santiago.aranguri@gmail.com>
2018-07-17 10:38:47 +0300

    Wavelets

--- neur.py
+++ neur.py
@@ -1,3 +1,5 @@
+import time as time_mod
+import pycorrelate as pyc
 import scipy.io
 import pywt
 import matplotlib.pyplot as plt
@@ -52,13 +54,14 @@ def correlate1():
     window = 10 / (planck_time * bin_size)
     sum = 0
 
-    for i in range(0, 5):
+    for i in range(3, 4):
     #for i, j in [(0, 2), (1, 0), (2, 1), (3, 4), (4, 3)]:
         correlation = correlate(data[i]['Vm'], data[i]['Fluorescence'], bin_size)
         half = len(correlation) / 2
         correlation = correlation[int(half - window):int(half + window)]
         time = np.arange(-len(correlation) / 2, len(correlation) / 2, 1) * planck_time * bin_size
         plt.plot(time, correlation)
+    plt.legend(files)
     plt.xlabel('Delay from axon activity to cell activity (s)')
     plt.ylabel('Correlation')
     plt.show()
@@ -109,14 +112,65 @@ def plot_data(smoothed=False):
     plot_two_figs(vm_vector, fl_vector, time_vector)
     plt.show()
 
-def wt_test():
-    vectors = data[1]
-    vm = vectors['Vm'][0:10000]
-    fl = vectors['Fluorescence'][0:10000]
-    time = vectors['Time'][0:10000]
-    plot_two_figs(vm, fl, time)
+def wt_range(start, end):
+    #return np.arange(2500 / end, 2500 / start, 100)
+    #return np.logspace(1.4, 3.7)
+    return np.logspace(2.4, 4.4)
 
-    coefs, freqs = pywt.cwt(vm, np.arange(1, 129), 'mexh', .0025)
+def wt_test():
+    vectors = data[0]
+    size = len(vectors['Vm'])
+    padding = int(size / 10)
+    vm = vectors['Vm'][0:size]
+    fl = vectors['Fluorescence'][0:size]
+    time = vectors['Time'][0:size]
+    #plot_two_figs(vm, fl, time)
+    print(size)
+
+    coefs, freqs = pywt.cwt(vm, wt_range(.5, 100), 'mexh', .0001)
+    print (freqs)
+    flat_coefs = np.sum(coefs, axis=0)
+    plt.plot(flat_coefs)
+    plt.show()
+    '''
+    print (freqs)
+
+    freqs = [f * 10 for f in np.flip(freqs, 0)]
+    new_coefs = []#[[] for _ in range(10000)]
+    j = -1
+    current_freq = -1
+    for i in range(4, 1000):
+        if i > current_freq and j < (len(coefs) - 1):
+            j += 1
+            current_freq = freqs[j]
+        new_coefs.append(coefs[j])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.imshow(new_coefs, interpolation='nearest', aspect='auto')
+
+    #flat_coefs100 = np.sum(coefs100, axis=0)
+    #flat_coefs10 = np.sum(coefs10, axis=0)
+
+    #plt.matshow(coefs10)
+    #plt.plot(flat_coefs30[padding:-padding])
+    #plt.plot(flat_coefs10[padding:-padding])
+    #plt.plot(flat_coefs100[padding:-padding])
+    #plt.legend(['0-30 Hz', '30-100 Hz'])
+    plt.show(block=False)
+    time_mod.sleep(10000)
+
+    plt.figure(1)
+    plt.matshow(coefs1)
+    plt.yticks([0])#30, 31, 33, 36, 42, 50, 65, 78, 100])
+    plt.figure(2)
+    plt.matshow(coefs2)
+    plt.yticks([0])#0, 1, 2, 4, 8, 16, 30])
+    plt.show()
+    #ax.set_xticklabels(np.arange(0, size*planck_time, planck_time * 1000))
+    #ax.set_yticklabels(freqs2)
+
+    #print (np.shape(coefs), np.shape(freqs))
     freqs = [int(freq * 100) for freq in freqs]
 
     j = 0
@@ -128,12 +182,15 @@ def wt_test():
 
     plt.matshow(new_coefs, aspect='auto')
     plt.show()
+    '''
 
 def correlate(array1, array2, bin_size):
     array1 = np.mean(array1.reshape(-1, bin_size), axis=1)
     array2 = np.mean(array2.reshape(-1, bin_size), axis=1)
     array1 = (array1 - np.mean(array1)) / (np.std(array1) * len(array1))
     array2 = (array2 - np.mean(array2)) / (np.std(array2))
+    print (len(array1))
+    print (len(array2))
     return np.correlate(array1, array2, 'same')
 
 def plot_two_figs(vector1, vector2, time):
@@ -144,11 +201,20 @@ def plot_two_figs(vector1, vector2, time):
     plt.plot(time, vector2)
 
 def near_zero():
-    window = int(1 / planck_time)
-    for i in range(0, 5):
-        correlation = correlate(data[i]['Vm'], data[i]['Fluorescence'], 1)
+    window = .5 / planck_time
+    bin_size = 10
+    window = int(window / bin_size)
+    for i in range(4, 5):
+        correlation = correlate(data[i]['Vm'], data[i]['Fluorescence'], bin_size)
         half = int(len(correlation) / 2)
         correlation = correlation[half - window:half + window]
+        best_point = -100
+        best_time = None
+        for j, point in enumerate(correlation):
+            if point > best_point:
+                best_point = point
+                best_time = j
+        print (i, best_time - window, best_point)
         plt.plot(correlation)
     plt.show()
 
@@ -156,5 +222,8 @@ init()
 #correlate1()
 #plot_data(True)
 #covariance()
-#wt_test()
-near_zero()
+wt_test()
+#near_zero()
+
+
+#Make an average of the columns
